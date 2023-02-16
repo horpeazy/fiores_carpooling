36e922b288ce5b240b10b0f497e10354a741c781
Hope Iyamu <iyamuhope.nosa647@gmail.com>
2023-02-16 21:49:33 +0000

    fix: local

--- carpool/settings.py
+++ carpool/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x7q=#k#6pox7ncsu23gp-k2a-3j3)6*16^q-zegsa(p864koyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
